42998c7681288f8bc651cb3f8caa87fc3052875f
Guillaume AUBERT <37863432+MySoftwareHub@users.noreply.github.com>
2026-01-22 22:08:04 +0100

    Add files via upload

---
+++ Arduino/slave/slave.ino
@@ -0,0 +1,38 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial ArduinoMaster(2,3);
+String msg;
+
+void setup(){
+  //Serial.begin(9600);
+  ArduinoMaster.begin(9600);   
+  pinMode(13,OUTPUT) ; 
+   pinMode(9,OUTPUT) ; 
+}
+
+void loop(){
+  readSerialPort();
+
+  if(msg=="0"){
+    digitalWrite(13,LOW) ;
+     digitalWrite(9,LOW) ;
+    msg=""; 
+  }
+  if(msg=="1"){
+    digitalWrite(13,HIGH) ;
+     digitalWrite(9,HIGH) ;
+    msg=""; 
+  }
+}
+
+void readSerialPort(){
+ while (ArduinoMaster.available()) {
+   delay(10); 
+   if (ArduinoMaster.available() >0) {
+     char c = ArduinoMaster.read();  //gets one byte from serial buffer
+     msg += c; //makes the string readString
+   }
+ }
+ ArduinoMaster.flush();
+}
+
